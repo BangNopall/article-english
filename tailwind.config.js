c1bfcda214d18b7f0b1c024c3f940a5847385c93
BangNopall <craft6254@gmail.com>
2024-11-27 16:19:05 +0700

    update baru

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#00ADB5',
+        secondary: '#222831',
+        tertiary: '#EEEEEE',
+      },
+      fontFamily: {
+        righteous: ['Righteous'],
+        poppins: ['Poppins', 'sans-serif'],
+        timesnewroman: ['timesnewroman']
+      },
+      dropShadow: {
+        'neon': '0 0px 15px rgba(0, 173, 181,0.50)',
+      },
+      boxShadow: {
+        'neon': '0 0px 35px rgba(0, 173, 181,0.50)',
+        'pressed': 'inset 12px 12px 24px #9c9c9c, inset -12px -12px 24px #9c9c9c',
+      },
+    },
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [
+    require('flowbite/plugin'), 
+    require('flowbite-typography'),
+  ],
 }
 
